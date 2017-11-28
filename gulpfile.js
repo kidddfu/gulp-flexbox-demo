a2712f0fd00f43583899f9e6be31860cb95367d6
Marble <joshuazack@icloud.com>
2017-11-29 03:10:55 +0800

    3th commit
    加入了gulp-wrap使得复用更加容易并更新项目结构，加入了browserSync使项目自动化。

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,39 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minify = require('gulp-minify-css');
+var wrap = require('gulp-wrap');
+var browserSync = require('browser-sync');
+
+// browserSync
+gulp.task('browser-sync', ['build', 'sass'], function(){
+    browserSync({
+        server: {
+            baseDir: 'dist/'
+        }
+    })
+});
+gulp.task('rebuild', ['build'] ,function(){
+    browserSync.reload();
+});
+
+// gulpwrap
+gulp.task('build', function(){
+    gulp.src('src/works.html')
+        .pipe(wrap({src:"src/layout/default.html"}))        
+        .pipe(gulp.dest('dist/'));
+    gulp.src('src/blog.html')
+        .pipe(wrap({src:"src/layout/default.html"}))        
+        .pipe(gulp.dest('dist/'));
+    gulp.src('src/index.html')
+        .pipe(wrap({src:"src/layout/default.html"}))        
+        .pipe(gulp.dest('dist/'));
+});
+
+// handleError防止错误中断gulp,加了handleError可报错后重连
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
 
 /*图片压缩 */
 var imagemin = require('gulp-imagemin');
@@ -21,22 +54,22 @@ gulp.task('imagemin', function(){
 /*主管道 */
 gulp.task('sass', function(){
     gulp.src('src/main.scss')
-        .pipe(sass())
+        .pipe(sass()).on('error', handleError)
         .pipe(autoprefixer())
         .pipe(minify())
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'))
+        // 监听修改
+        .pipe(browserSync.reload({stream: true}));
 });
 
-gulp.task('copy-assets', function(){
-    gulp.src('src/*.html')
-        .pipe(gulp.dest('dist/'));
-});
+
 
 /*监控、热加载*/
 gulp.task('watch', function(){
-    gulp.watch(['src/index.html'], ['copy-assets']);
-    gulp.watch(['src/main.scss'], ['sass']);
+    gulp.watch(['src/**/*.html'], ['rebuild']);
+    gulp.watch(['src/*.scss'], ['sass']);
 })
 
-gulp.task('default', ['sass', 'copy-assets', 'watch']);
+// 有了browsersync，不需要sass，build了—— gulp.task('default', ['sass', 'build', 'watch']);
+gulp.task('default', ['browser-sync', 'watch']);
 
